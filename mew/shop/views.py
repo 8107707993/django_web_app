6cb520065e0f2f863982804ff7769e477396864c
subhash sharma <subhash810770yahoo.com>
2020-11-23 13:07:26 +0530

    set catagery

--- mew/shop/views.py
+++ mew/shop/views.py
@@ -1,17 +1,30 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from .models import Product
 from math import ceil
 
-
 # Create your views here.
+from django.http import HttpResponse
+
 
 def index(request):
-    products = Product.objects.all()
-    print(products)
-    n = len(products)
-    nSlides = n // 4 + ceil((n / 4) - (n // 4))
-    params = {'no_of_slides': nSlides, 'range': range(1, nSlides), 'product': products}
+    # products = Product.objects.all()
+    # print(products)
+    # n = len(products)
+    # nSlides = n//4 + ceil((n/4)-(n//4))
+
+    allProds = []
+    catprods = Product.objects.values('category', 'id')
+    cats = {item['category'] for item in catprods}
+    for cat in cats:
+        prod = Product.objects.filter(category=cat)
+        n = len(prod)
+        nSlides = n // 4 + ceil((n / 4) - (n // 4))
+        allProds.append([prod, range(1, nSlides), nSlides])
+
+    # params = {'no_of_slides':nSlides, 'range': range(1,nSlides),'product': products}
+    # allProds = [[products, range(1, nSlides), nSlides],
+    #             [products, range(1, nSlides), nSlides]]
+    params = {'allProds': allProds}
     return render(request, 'shop/index.html', params)
 
 
@@ -20,20 +33,20 @@ def about(request):
 
 
 def contact(request):
-    return render(request, 'shop/contact.html')
+    return HttpResponse("We are at contact")
 
 
 def tracker(request):
-    return HttpResponse("hello Tracker")
+    return HttpResponse("We are at tracker")
 
 
 def search(request):
-    return HttpResponse("hello Search")
+    return HttpResponse("We are at search")
 
 
-def prodView(request):
-    return HttpResponse("hello Productview")
+def productView(request):
+    return HttpResponse("We are at product view")
 
 
 def checkout(request):
-    return HttpResponse("hello Checkout")
+    return HttpResponse("We are at checkout")
